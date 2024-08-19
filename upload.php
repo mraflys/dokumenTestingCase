86dba9a0a76cce80dfc4a260080b8c69d6406701
raflysyah.putra <raflysyah.putra@ilcs.co.id>
2024-08-19 21:57:39 +0700

    push data

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['submit'])) {
-    $target_dir = "uploads/";
+    $target_dir = "./uploads/";
     
     // Create target directory if it doesn't exist
     if (!file_exists($target_dir)) {
